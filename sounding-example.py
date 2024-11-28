dee984ca8e5476abc817a219c85ccd0b3d9c6ff4
SteveFroehlich <froehlich@uwalumni.com>
2024-11-28 18:24:59 +0000

    make print statements useful

--- sounding-example.py
+++ sounding-example.py
@@ -3,14 +3,14 @@ from matplotlib import pyplot as plt
 
 import act
 
-print("test")
+print("imports complete")
 
 # Read data
 filename_sonde = DATASETS.fetch('sgpsondewnpnC1.b1.20190101.053200.cdf')
-print("test 2")
+print("data loaded")
 
 ds = act.io.arm.read_arm_netcdf(filename_sonde)
-print("test 3")
+print("data formatted")
 
 # Plot enhanced Skew-T plot
 display = act.plotting.SkewTDisplay(ds)
